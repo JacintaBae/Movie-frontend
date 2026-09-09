a75e55722f59c85a81e620e750dbcb1664a5b7fc
jacinta <ogochukwujacinta18@gmail.com>
2026-09-09 15:56:03 +0100

    connected to be

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://18.221.67.127:8080',
+    baseURL:'http://100.25.166.213:8080',
     headers: {
         'Content-Type': 'application/json',
     },
