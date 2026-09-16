bf83b68d5cce18cfb298436e9b06ddbf8d7dc2cd
jacinta <ogochukwujacinta18@gmail.com>
2026-09-16 12:18:53 +0100

    docker compose with FE

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://100.25.166.213:8080',
+    baseURL:'http://100.26.212.146:8080',
     headers: {
         'Content-Type': 'application/json',
     },
 });
+
+
